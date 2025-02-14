e7870ef16dbb290b00efbd48d8af926d9f4046bb
Aleksey Tyurin <alaxche@gmail.com>
2025-02-14 14:31:25 +1000

    API working

--- DB/services/comments.tsx
+++ DB/services/comments.tsx
@@ -4,7 +4,6 @@ import { Connection } from 'mysql2/promise';
 export async function getCommentsByPostId(req: NextApiRequest, res: NextApiResponse, connection: Connection, postId: string) {
     try {
         const [comments] = await connection.query('SELECT * FROM comments WHERE postId = ?', [postId]);
-        console.log(comments)
         res.status(200).json(comments);
     } catch (error) {
         console.error(error);
@@ -25,5 +24,60 @@ export async function createComment(req: NextApiRequest, res: NextApiResponse, c
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Ошибка создания комментария" });
+    }
+}
+
+export async function getCommentById(req: NextApiRequest, res: NextApiResponse, connection: Connection, id: string) {
+    try {
+        const [comment] = await connection.query('SELECT * FROM comments WHERE id = ?', [id]) 
+
+        if (comment.length === 0) {
+            return res.status(404).json({ message: "Комментарий не найден" });
+        }
+
+        res.status(200).json(comment[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Ошибка получения комментария" });
+    }
+}
+
+export async function updateCommentById(req: NextApiRequest, res: NextApiResponse, connection: Connection, id: string) {
+    try {
+        const [commentById] = await connection.query('SELECT * FROM comments WHERE id = ?', [id]);
+        if (commentById.length === 0) {
+            return res.status(404).json({ message: "Комментарий не найден" });
+        }
+
+        const { comment } = req.body;
+
+        const updateCommentQuery = `
+            UPDATE comments 
+            SET 
+                comment = COALESCE(?, comment)
+            WHERE id = ?;
+        `;
+
+        await connection.query(updateCommentQuery, [comment, id]);
+        const [updatedComment] = await connection.query('SELECT * FROM comments WHERE id = ?', [id]);
+
+        res.status(200).json(updatedComment)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Ошибка при обновлении комментария" });
+    }
+}
+
+export async function deleteComment(req: NextApiRequest, res: NextApiResponse, connection: Connection, id: string) {
+    try {
+        const deleteCommentQuery = `
+            DELETE FROM comments WHERE id = ?;
+        `;
+        await connection.query(deleteCommentQuery, [id]);
+
+        return res.status(200).json({ message: "Комментарий успешно удален" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Ошибка при удалении комментария" });
     }
 }
--- pages/api/posts/comments/[id].tsx
+++ pages/api/posts/comments/[id].tsx
@@ -1,17 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import connectToDatabase from '@/DB/utils/db';
-import { getPostById, updatePost, deletePost } from '@/DB/services/posts';
+import { getCommentById, updateCommentById, deleteComment } from '@/DB/services/comments';
 
 export default async function(req: NextApiRequest, res: NextApiResponse) {
     const connection = await connectToDatabase();
     const {id} = req.query;
     
     if (req.method === 'GET') {
-        await getPostById(req, res, connection, id as string);
+        await getCommentById(req, res, connection, id as string);
     } else if (req.method === 'PATCH') {
-        await updatePost(req, res, connection, id as string);
+        await updateCommentById(req, res, connection, id as string);
     } else if (req.method === 'DELETE') {
-        await deletePost(req, res, connection, id as string);
+        await deleteComment(req, res, connection, id as string);
     } else {
         res.status(405).json({ message: "Метод не разрешен" });
     }
